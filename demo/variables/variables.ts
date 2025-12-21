98bc69dc868d3dd45b07d8b1422688e16c6ed287
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-21 12:22:45 -0400

    Add additional variable declarations for numeric values in variables.ts

--- demo/variables/variables.ts
+++ demo/variables/variables.ts
@@ -2,5 +2,11 @@
 
 const courseName = "typescript";
 const courseName2 = 'typescript';
-
 const courseName3 = `typescript`;
+
+const amount = 10;
+const fraction = 10.5;
+const oneThousand = 1e3;
+const allPermissions = 0o777;
+
+
