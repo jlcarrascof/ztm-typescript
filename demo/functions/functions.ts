5962473f50385f0a5e5aa84300231908bbab4e82
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-01-07 13:57:31 -0400

    Add additional assertions for sum calculations

--- demo/functions/functions.ts
+++ demo/functions/functions.ts
@@ -19,3 +19,8 @@ assert.equal(answer2, 6);
 
 const answer3 = sum(sum(1, 1), sum(2, 2));
 assert.equal(answer3, 6);
+
+const lsh = sum(1, 1);
+const rhs = sum(2, 2);
+const answer4 = sum(lsh, rhs);
+assert.equal(answer4, 6);
